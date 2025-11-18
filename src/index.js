c9611f4461d404919351d1837e08a112bc5257fc
MCurb <marcoscurbeco03@gmail.com>
2025-11-18 09:53:48 -0600

    feat: create capitalize function
    This function takes a string as input and returns the string with the
    first letter capitalized.

---
+++ src/index.js
@@ -0,0 +1,7 @@
+function capitalize(word) {
+  const firstLetter = word.slice(0, 1).toUpperCase();
+  const rest = word.slice(1);
+  return firstLetter.concat('', rest);
+}
+
+module.exports = { capitalize };
