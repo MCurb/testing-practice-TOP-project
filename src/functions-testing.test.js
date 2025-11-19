02faa258dd3329a6742277c9635172b755e4b482
MCurb <marcoscurbeco03@gmail.com>
2025-11-18 20:33:30 -0600

    feature: add analyze array test

--- src/functions-testing.test.js
+++ src/functions-testing.test.js
@@ -66,3 +66,14 @@ test('test case perseveration', () => {
 test('test punctuation', () => {
   expect(caesarCipher('Hello, World!', 3)).toBe('Khoor, Zruog!');
 });
+
+//Analize Array
+
+test('test object analisis', () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  });
+});
